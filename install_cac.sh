6c5d8a531e47eae74902fb8abfbcd0aff9356845
Rick <422322@users.noreply.github.com>
2019-01-22 13:10:03 +0100

    Update install_cac.sh

--- install_cac.sh
+++ install_cac.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -ex
-apt-get -y update
-apt-get install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-apt-get install -y npm
-apt-get install -y nodejs
-apt-get install -y git
+sudo apt-get -y update
+sudo apt-get install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo apt-get install -y npm
+sudo apt-get install -y nodejs
+sudo apt-get install -y git
 mkdir /home/webapp
 cd /home/webapp
 git clone https://github.com/TimothySealy/cac-simple-webapp.git
 cd cac-simple-webapp
 echo PORT=80 >> /etc/environment
 export PORT=80
-npm install
-npm run start &
+sudo npm install
+sudo npm run start &
