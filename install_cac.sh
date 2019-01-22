c2b2554a7a153ed6dabc57ee9d22a1900d7a94a6
Rick <422322@users.noreply.github.com>
2019-01-22 13:50:13 +0100

    Update install_cac.sh

--- install_cac.sh
+++ install_cac.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -ex
 sudo apt-get -y update
-sudo apt-get install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo apt-get install -y npm
 sudo apt-get install -y nodejs
 sudo apt-get install -y git
