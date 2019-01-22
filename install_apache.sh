5a8ec3de97dea56503887e928bab35ac40577b20
Rick <422322@users.noreply.github.com>
2019-01-22 12:01:58 +0100

    Apache met demo page voor Azure ARM VM

--- install_apache.sh
+++ install_apache.sh
@@ -2,10 +2,10 @@
 sudo apt-get -y update
 
 # install Apache2
-sudo apt-get -y install apache2 
+sudo apt-get -y install apache2 #alles wordt goed uitgevoerd, behalve dit commando. Daarom werkt het volgende commando ook niet, want /var/www/html/ bestaat niet.
 
 # write some HTML
-sudo echo \<center\>\<h1\>Solid Partners\</h1\>\<br/\>\<br/\>Demo pagina\<br/\>\</center\> > /var/www/html/demo.html
+sudo echo \<center\>\<h1\>Solid Partners\</h1\>\<br/\>\<br/\>Demo pagina\<br/\>\</center\> > /var/www/html/index.html
 
 # restart Apache
 sudo apachectl restart
