d6955433828ee39fb1db7ced072830eb69b064c2
Rick <422322@users.noreply.github.com>
2019-01-22 12:27:04 +0100

    Update install_cac.sh

--- install_cac.sh
+++ install_cac.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
-sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum install -y npm
-sudo yum install -y nodejs
-sudo yum install -y git
+sudo apt-get install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo apt-get install -y npm
+sudo apt-get install -y nodejs
+sudo apt-get install -y git
 sudo mkdir /home/webapp
 sudo cd /home/webapp
 sudo git clone https://github.com/TimothySealy/cac-simple-webapp.git
