61e408614c9d2233bc4b53f3db46469b082381df
Rick <422322@users.noreply.github.com>
2019-01-23 12:07:46 +0100

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -4,9 +4,6 @@ sudo apt-get -y update
 # installeer Apache2
 sudo apt-get -y install apache2 #alles wordt goed uitgevoerd, behalve dit commando. Daarom werkt het volgende commando ook niet, want /var/www/html/ bestaat niet.
 
-# sudo rechten
-sudo -i
-
 # HTML demo pagina maken
 sudo echo \<center\>\<h1\>Solid Partners\</h1\>\<br/\>\<br/\>Demo pagina\<br/\>\</center\> > /var/www/html/index.html
 
