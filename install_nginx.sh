f6a338d112247b6b57f9706daca51012730a7719
Rick <422322@users.noreply.github.com>
2019-01-23 13:56:32 +0100

    Installeer NGINX met demo pagina

--- install_nginx.sh
+++ install_nginx.sh
@@ -3,9 +3,6 @@ sudo apt-get -y update
 
 # installeer NGINX
 sudo apt-get -y install nginx
-#alles wordt goed uitgevoerd, behalve dit commando. Daarom werkt het volgende commando ook niet, want /var/www/html/ bestaat niet.
-
-sudo chmod 777 /var/www/html/index.html
 
 # HTML demo pagina maken
 sudo echo \<center\>\<h1\>Solid Partners\</h1\>\<br/\>\<br/\>Demo pagina\<br/\>\</center\> > /usr/share/nginx/html/index.html
