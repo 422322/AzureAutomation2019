413e053c7b438914834ef966aea1fbf78be85942
Rick <422322@users.noreply.github.com>
2019-01-23 15:18:43 +0100

    Azure Ubuntu VM met NGINX en demo/hostname pagina

--- install_nginx.sh
+++ install_nginx.sh
@@ -5,7 +5,7 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 
 # HTML demo pagina maken
-sudo echo \<center\>\<h1\>Solid Partners\</h1\>\<br/\>\<br/\>Demo pagina vanaf server \<b\>`hostname`\</b\> op $(date +%d-%m-%Y) om $(date +%H:%M:%S)\<br/\>\</center\> > /usr/share/nginx/html/index.html
+sudo echo \<center\>\<h1\>Solid Partners\</h1\>\<br/\>\<br/\>Demo pagina vanaf server \<b\>`hostname`\</b\>\<br/\>\</center\> > /usr/share/nginx/html/index.html
 
 # herstart NGINX
 service nginx restart
