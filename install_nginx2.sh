4362f98be302c8b454bcfc2ff8693341e2ea6cec
Rick <422322@users.noreply.github.com>
2019-01-24 10:26:39 +0100

    Azure Ubuntu VM met NGINX en demo pagina (2)

--- install_nginx2.sh
+++ install_nginx2.sh
@@ -5,7 +5,7 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 
 # HTML demo pagina maken
-sudo echo \<center\>\<h1\>Solid Partners\</h1\>\<br/\>\<br/\>Demo pagina vanaf server \<b\>\<script\>var myArray \= \[\"MyUbuntuVM1\"\,\"MyUbuntuVM2\"\,\"MyUbuntuVM3\"\]\;var randomItem \= myArray\[Math\.floor\(Math\.random\(\)\*myArray\.length\)\]\;document\.body\.innerHTML \= randomItem\;\</script\>\</b\>\<br/\>\</center\> > /usr/share/nginx/html/index.html
+sudo echo \<center\>\<h1\>Solid Partners\</h1\>\<br/\>\<br/\>Demo pagina vanaf server \<b\>\<script\>var hostname \= \[\'MyUbuntuVM1\'\, \'MyUbuntuVM2\'\]\;var tekst \= hostname\[Math\.floor\(Math\.random\(\) \* hostname\.length\)\]\;document\.write\(tekst\)\;\</script\>\</b\>\<br/\>\</center\> > /usr/share/nginx/html/index.html
 
 # herstart NGINX
 service nginx restart
